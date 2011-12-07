4ac26fdaf24e93bbe9128ca834468576273bde08
Andrew Stone <andy@stonean.com>
2011-12-07 09:07:06 -0500

    remove asset_id functionality.

--- lib/sinatra/pubba.rb
+++ lib/sinatra/pubba.rb
@@ -9,20 +9,10 @@ module Sinatra
     def self.registered(app)
       if app.settings.development? || app.settings.test?
         Site.configure(app)
-
-        app.before do
-          app.settings.set :asset_id, ->{Time.now.strftime("%N")}
-        end
-      else
-        app.settings.set :asset_id, asset_id
       end
 
       app.helpers Sinatra::Pubba::HTML::Helpers
 
-    end
-
-    def self.asset_id
-      ENV['ASSET_ID'] || raise(ConfigurationError, "Required ENV[ASSET_ID] not defined")
     end
   end # Pubba
 end # Sinatra
