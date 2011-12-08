47fc10c4f53b45dc1949085b242bfcae2b526118
Andrew Stone <andy@stonean.com>
2011-12-08 15:21:43 -0500

    move processing on config file into before filter in dev and test mode. always configure

--- lib/sinatra/pubba.rb
+++ lib/sinatra/pubba.rb
@@ -7,12 +7,15 @@ require_relative 'pubba/html/helpers'
 module Sinatra
   module Pubba
     def self.registered(app)
+      Site.configure(app)
+
       if app.settings.development? || app.settings.test?
-        Site.configure(app)
+        app.before do
+          Site.process(app)
+        end
       end
 
       app.helpers Sinatra::Pubba::HTML::Helpers
-
     end
   end # Pubba
 end # Sinatra
