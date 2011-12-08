792feb86bf3f7e329aaef595646b82001d7fcad0
Andrew Stone <andy@stonean.com>
2011-12-08 15:22:47 -0500

    capture config filename

--- lib/sinatra/pubba/assets/configuration.rb
+++ lib/sinatra/pubba/assets/configuration.rb
@@ -5,13 +5,15 @@ module Sinatra
     module Assets
       class Configuration
         attr_reader :yaml
+        attr_reader :name
 
         def initialize(config_file)
+          @name = File.basename(config_file)
           @yaml = Psych.load_file(config_file)
         end
 
         def global_config!
-          yaml.delete("global") || {}
+          @global_config ||= (yaml.delete("global") || {})
         end
 
         def process
