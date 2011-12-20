d840848bb3403b10ad33cafd55336719a646d98e
Andrew Stone <andy@stonean.com>
2011-12-20 09:09:28 -0500

    use Rack::StaticaServer

--- lib/sinatra/pubba.rb
+++ lib/sinatra/pubba.rb
@@ -1,4 +1,5 @@
 require 'sinatra/base'
+require 'rack/statica_server'
 
 require_relative 'pubba/errors'
 require_relative 'pubba/site'
@@ -7,6 +8,10 @@ require_relative 'pubba/html/helpers'
 module Sinatra
   module Pubba
     def self.registered(app)
+      unless app.settings.test?
+        use Rack::StaticaServer
+      end
+
       Site.configure(app)
 
       app.helpers Sinatra::Pubba::HTML::Helpers
